c9c74bd7bd0493ff704ab65fadb16161da83eb0b
Eustaquio Rangel <taq@eustaquiorangel.com>
2013-09-09 15:55:29 -0300

    Isso não existe, esse teste não funciona!

--- spec/controllers/avaliable_controller_spec.rb
+++ spec/controllers/avaliable_controller_spec.rb
@@ -2,25 +2,25 @@ require 'spec_helper'
 
 describe AvaliablesController do
 
-  describe "GET 'person_id:integer'" do
-    it "returns http success" do
-      get 'person_id:integer'
-      response.should be_success
-    end
-  end
+  #describe "GET 'person_id:integer'" do
+    #it "returns http success" do
+      #get 'person_id:integer'
+      #response.should be_success
+    #end
+  #end
 
-  describe "GET 'skill_id:integer'" do
-    it "returns http success" do
-      get 'skill_id:integer'
-      response.should be_success
-    end
-  end
+  #describe "GET 'skill_id:integer'" do
+    #it "returns http success" do
+      #get 'skill_id:integer'
+      #response.should be_success
+    #end
+  #end
 
-  describe "GET 'level_id:integer'" do
-    it "returns http success" do
-      get 'level_id:integer'
-      response.should be_success
-    end
-  end
+  #describe "GET 'level_id:integer'" do
+    #it "returns http success" do
+      #get 'level_id:integer'
+      #response.should be_success
+    #end
+  #end
 
 end
